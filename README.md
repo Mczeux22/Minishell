b76b11d5565d3e6027040258e3edbd0f383ac9b6
loic <paponloic@outlook.fr>
2026-04-29 08:33:10 +0000

    t

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Minishell
+
+Note de dev : 
+
+Source - 
+
+https://medium.com/@laamirimarouane8/42-minishell-guide-53600f49742b
+https://medium.com/@laamirimarouane8/42-minishell-guide-53600f49742b
+https://docs.google.com/spreadsheets/d/1uJHQu0VPsjjBkR4hxOeCMEt3AOM1Hp_SmUzPFhAH-nA/edit?gid=0#gid=0
+https://github.com/Hqndler/42-minishell/blob/main/README.md
+
+
+
+Tech utiliser
+
+- AST - > abstract syntax tree
+- Tokenization
+- Fork
+- exec()
+- Pipes
--- include/minishell.h
+++ include/minishell.h
@@ -1,13 +1,24 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: Lopapon <lopapon@student.42perpignan.fr    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/23 15:03:23 by Lopapon           #+#    #+#             */
-/*   Updated: 2025/05/23 15:03:34 by Lopapon          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/**
+ * @ Author: Lopapon
+ * @ Create Time: 2026-04-29 08:07:08
+ * @ Modified by: Lopapon
+ * @ Modified time: 2026-04-29 08:22:31
+ * @ Description:
+ */
 
+#ifndef MINISHELL_H
+ # define MINISHELL_H
+
+ //Lib globale
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <fcntl.h>
+# include <sys/stat.h>
 #include "libft.h"
+
+
+#endif
